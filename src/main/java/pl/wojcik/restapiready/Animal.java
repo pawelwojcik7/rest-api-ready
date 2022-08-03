f7cebeab5d81c9494ef31fc291d82c9be1444655
pawel.wojcik7 <pawel.wojcik7@pollub.edu.pl>
2022-08-03 18:56:52 +0200

    Commit dawid 4

--- src/main/java/pl/wojcik/restapiready/Animal.java
+++ src/main/java/pl/wojcik/restapiready/Animal.java
@@ -11,6 +11,7 @@ import javax.persistence.Id;
 @Getter
 @Setter
 @Entity
+
 public class Animal {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
--- src/main/java/pl/wojcik/restapiready/PersonService.java
+++ src/main/java/pl/wojcik/restapiready/PersonService.java
@@ -18,4 +18,9 @@ public class PersonService {
         System.out.println("Dawid2");
 
     }
+
+    public void function4(){
+        System.out.println("Dawid4");
+
+    }
 }
